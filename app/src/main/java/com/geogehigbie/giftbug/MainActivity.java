58d9e0d3eaa5e24415bd1900f783e1ced39ced63
George Higbie <gehigbie@gmail.com>
2016-11-27 19:23:10 -0700

    changed fragments

--- app/src/main/java/com/geogehigbie/giftbug/MainActivity.java
+++ app/src/main/java/com/geogehigbie/giftbug/MainActivity.java
@@ -1,14 +1,31 @@
 package com.geogehigbie.giftbug;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import layout.PersonIntro;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fragmentInitialize(){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new PersonIntro());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+
     }
 
 
