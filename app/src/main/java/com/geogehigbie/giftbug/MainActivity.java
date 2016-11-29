2fe9f03bd74ae4ca1322c9c9133228399e7e284c
George Higbie <gehigbie@gmail.com>
2016-11-29 13:00:57 -0700

    added onClick listeners - need to add background fro fragments

--- app/src/main/java/com/geogehigbie/giftbug/MainActivity.java
+++ app/src/main/java/com/geogehigbie/giftbug/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import layout.PersonIntro;
 
@@ -16,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadIntroPage();
+    }
+
+    public void loadIntroPage(){
+        Button getStarted = (Button) findViewById(R.id.get_started);
+
+        getStarted.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                fragmentInitialize();
+            }
+        });
     }
 
     public void fragmentInitialize(){
@@ -24,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.add(R.id.fragment_container, new PersonIntro());
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
 
     }
 
