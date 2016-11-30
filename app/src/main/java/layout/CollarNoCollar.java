4659e3271d3642f94dda63064fb59a3d549d4e02
George Higbie <gehigbie@gmail.com>
2016-11-30 13:03:08 -0700

    set textview and button view

--- app/src/main/java/layout/CollarNoCollar.java
+++ app/src/main/java/layout/CollarNoCollar.java
@@ -7,10 +7,10 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.geogehigbie.giftbug.R;
-
-
 
 
 public class CollarNoCollar extends Fragment {
@@ -28,6 +28,9 @@ public class CollarNoCollar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        setTextView();
+        setOnClickListeners();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_collar_no_collar, container, false);
     }
@@ -37,11 +40,30 @@ public class CollarNoCollar extends Fragment {
 
         String wearsCollarString = "Does " + personLower + " usually wear a collar or no collar?";
 
+        TextView wearsCollarText  = (TextView) getActivity().findViewById(R.id.collar_no_collar);
+        wearsCollarText.setText(wearsCollarString);
+
 
     }
 
     public void setOnClickListeners(){
 
+        Button collarButton = (Button) getActivity().findViewById(R.id.collar_button);
+        Button noCollarButton = (Button) getActivity().findViewById(R.id.no_collar_button);
+
+        collarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wearsCollar = true;
+            }
+        });
+
+        noCollarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wearsCollar = false;
+            }
+        });
 
     }
 
