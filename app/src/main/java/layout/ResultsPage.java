f7efc1dbae20d83d40095e39256710be3eb08bb4
George Higbie <gehigbie@gmail.com>
2016-12-01 16:32:26 -0500

    changed the logo and splash page

--- app/src/main/java/layout/ResultsPage.java
+++ app/src/main/java/layout/ResultsPage.java
@@ -2,20 +2,45 @@ package layout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.geogehigbie.giftbug.R;
 
 
 public class ResultsPage extends Fragment {
 
+    private View view;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private static String personLower;
+    private static String personUpper;
+    private static boolean hasYChromosome;
+    private static boolean isAdult;
+
+    private static boolean prefersBurgers;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_results_page, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_results_page, container, false);
+        return view;
+    }
+
+    public void setTextView(){
+
+        String resultsString = "Here are the results that we think " + personLower + " will love. (Clicking on an item will take you directly to the item\'s amazon page.)";
+
+        TextView resultsText = (TextView) view.findViewById(R.id.results_text);
+        resultsText.setText(resultsString);
+
     }
 
 
