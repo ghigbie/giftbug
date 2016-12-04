04aaf5a1d73c39cd7c6fee824c4b2dec5ddb9ac6
George Higbie <gehigbie@gmail.com>
2016-12-04 00:36:54 -0500

    added animation to first paged - fixed R problem

--- app/src/main/java/com/geogehigbie/giftbug/MainActivity.java
+++ app/src/main/java/com/geogehigbie/giftbug/MainActivity.java
@@ -1,5 +1,6 @@
 package com.geogehigbie.giftbug;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -21,21 +22,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadIntroPage();
+       // loadIntroPage();
+
+        setOnClickListeners();
 
         //fragmentInitialize();
     }
 
-    public void loadIntroPage(){
-        Button getStarted = (Button) findViewById(R.id.get_started);
-
-        getStarted.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                fragmentInitialize();
-            }
-        });
-    }
+ //  public void loadIntroPage(){
+//        Button getStarted = (Button) findViewById(R.id.get_started);
+//
+//        getStarted.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                fragmentInitialize();
+//            }
+//        });
+ //   }
 
     public void fragmentInitialize(){
         fragmentManager = getSupportFragmentManager();
@@ -48,9 +51,54 @@ public class MainActivity extends AppCompatActivity {
 
     public void beginAnimation(){
         ImageView bug = (ImageView) findViewById(R.id.bug_spinner);
+        bug.animate().rotation(360).setDuration(750).start();
+        bug.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
 
-        bug.rotationX(80).setDuration(400).start();
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                fragmentInitialize();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
     }
+
+
+     public void setOnClickListeners(){
+
+         Button english = (Button) findViewById(R.id.english);
+         Button español = (Button) findViewById(R.id.español);
+
+         english.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View view) {
+                 beginAnimation();
+             }
+         });
+
+         español.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View view) {
+                 beginAnimation();
+             }
+         });
+
+
+}
+
 
 
 }
