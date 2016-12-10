1caf44d87c02e092f0ba4688d5d6b4dc6a9d3dee
George Higbie <gehigbie@gmail.com>
2016-12-10 16:53:01 -0500

    working with recycler view

---
+++ app/src/main/java/com/geogehigbie/giftbug/RecyclerAdapter.java
@@ -0,0 +1,29 @@
+package com.geogehigbie.giftbug;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by georgehigbie on 12/9/16.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter {
+
+    String[] names, capital
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    //should I use list view
+}
